b921e45e82b20db3cd118876b450673181ffeda2
Raj000007 <rajeshraj19981@gmail.com>
2024-10-18 07:16:16 +0530

    first commit

---
+++ src/main/java/com/financeme/BankingFinanceApplication.java
@@ -0,0 +1,11 @@
+package com.financeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingFinanceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingFinanceApplication.class, args);
+    }
+}
