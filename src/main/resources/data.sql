cdb863264f8c7d6c45371a0e2f51c29fd667b9d9
Raj000007 <rajeshraj19981@gmail.com>
2024-10-18 08:07:50 +0530

    first commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 -- Preload account data
 INSERT INTO account (account_number, account_holder_name, balance) 
-VALUES ('ACC001', 'John Doe', 5000.00),
-       ('ACC002', 'Jane Smith', 3000.00),
-       ('ACC003', 'Alice Brown', 12000.00);
+VALUES 
+    ('1001', 'John Doe', 5000.00),
+    ('1002', 'Jane Smith', 3000.00),
+    ('1003', 'Alice Brown', 12000.00);
