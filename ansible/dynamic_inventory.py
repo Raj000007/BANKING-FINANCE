5672af1fac0c089d33b08387cd488bdb16273bbe
Rajesh B T <103110767+Raj000007@users.noreply.github.com>
2024-10-19 19:59:12 +0530

    Update dynamic_inventory.py

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python
 
 import json
-import sys
+import subprocess
 
 def main():
-    # Define your inventory
+    # Get the output from Terraform
+    tf_output = json.loads(subprocess.check_output(['terraform', 'output', '-json']).decode('utf-8'))
+
+    # Construct the inventory structure
     inventory = {
-        "test": {  # Define the 'test' group
-            "hosts": ["54.198.212.8"],  # Your target host
-            "vars": {
-                "ansible_ssh_user": "ubuntu",  # Set SSH user to 'ubuntu'
-                "ansible_ssh_private_key_file": "/var/lib/jenkins/.ssh/finance.pem"  # Specify your private key
-            }
+        'test': {
+            'hosts': [tf_output['test_server_ip']['value']],
         },
-        "_meta": {
-            "hostvars": {
-                "54.198.212.8": {
-                    "ansible_host": "54.198.212.8",
-                    "ansible_ssh_user": "ubuntu",  # Ensure this is also set here
-                    "ansible_ssh_private_key_file": "/var/lib/jenkins/.ssh/finance.pem"  # Specify your private key here too
-                }
-            }
-        }
+        'production': {
+            'hosts': [tf_output['prod_server_ip']['value']],
+        },
     }
-    
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "--list":
-            print(json.dumps(inventory))
-        elif sys.argv[1] == "--host":
-            print(json.dumps(inventory["_meta"]["hostvars"][sys.argv[2]]))
-    else:
-        print("Usage: {} --list|--host <hostname>".format(sys.argv[0]))
+
+    # Print the inventory in the required format
+    print(json.dumps(inventory))
 
 if __name__ == '__main__':
     main()
