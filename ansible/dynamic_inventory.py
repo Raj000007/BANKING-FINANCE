d7392471bc609fa74d45d5004d79717412f80038
Rajesh B T <103110767+Raj000007@users.noreply.github.com>
2024-10-19 06:16:17 +0530

    Update dynamic_inventory.py

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
 import json
+import sys
 
 def main():
-    # Manually set the IP addresses for the servers
+    # Define your inventory
     inventory = {
-        'test': {
-            'hosts': ['54.198.212.8'],  # Test server IP
+        "test": {  # Define the 'test' group
+            "hosts": ["54.198.212.8"],  # Your target host
+            "vars": {
+                "ansible_ssh_user": "ubuntu",  # Set SSH user to 'ubuntu'
+                "ansible_ssh_private_key_file": "root/.ssh/id_rsa"  # Specify your private key
+            }
         },
-        'production': {
-            'hosts': ['54.237.251.49'],  # Production server IP
-        },
+        "_meta": {
+            "hostvars": {
+                "54.198.212.8": {
+                    "ansible_host": "54.198.212.8",
+                    "ansible_ssh_user": "ubuntu",  # Ensure this is also set here
+                    "ansible_ssh_private_key_file": "root/.ssh/id_rsa"  # Specify your private key here too
+                }
+            }
+        }
     }
-
-    # Print the inventory in JSON format
-    print(json.dumps(inventory))
+    
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--list":
+            print(json.dumps(inventory))
+        elif sys.argv[1] == "--host":
+            print(json.dumps(inventory["_meta"]["hostvars"][sys.argv[2]]))
+    else:
+        print("Usage: {} --list|--host <hostname>".format(sys.argv[0]))
 
 if __name__ == '__main__':
     main()
